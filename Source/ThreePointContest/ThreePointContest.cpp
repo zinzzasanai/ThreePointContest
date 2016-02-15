f7006f8c5fe1b8387d0e723cd7fd4afd1292c365
zinzzasanai <zinzzasanai@naver.com>
2016-02-15 18:39:54 +0900

    프로젝트 추가

---
+++ Source/ThreePointContest/ThreePointContest.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ThreePointContest.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThreePointContest, "ThreePointContest" );
---
+++ Source/ThreePointContest/ThreePointContestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ThreePointContestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THREEPOINTCONTEST_API AThreePointContestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
